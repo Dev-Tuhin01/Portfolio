1141103ceef07df7ff582a89815d482cb2591be4
dev-tuhin01 <dev.tuhin.1@gmail.com>
2023-12-03 08:47:39 +0530

    Created header and footer sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <h2>This is main section</h2>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <h2>This is Header</h2>;
+};
+
+export default Header;
